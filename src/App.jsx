819e234d7d419a4ad949a179f2e066c7cf981056
mirzakhalov03 <javohirmirzakhalov@gmail.com>
2025-04-03 18:53:22 +0500

    added: tg sdk

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,27 @@ import RouteController from './router'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify'
 import './App.css'
+import { useEffect } from 'react';
+import WebApp from '@twa-dev/sdk';
 
 function App() {
 
-  const location = useLocation()
+  useEffect(()=>{
+    WebApp.expand()
+  }, [])
 
-  const hiddenRoutes = ['profile', ]
 
   return (
-    <>
-      {/* <NavBar/> */}
+    <div className='' style={{
+      backgroundColor: WebApp.themeParams.bg_color || "#fff",
+      color: WebApp.themeParams.text_color || "#000",
+      padding: "20px",
+      minHeight: "100vh",
+    }}
+    >
       <RouteController/>
       <ToastContainer/>
-    </>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import WebApp from '@twa-dev/sdk'
+
+WebApp.ready()
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
